001e2a5666e58694c4007e5cbcf1baf650111ca6
erisol <e.solhaug@stud.uis.no>
2018-10-16 12:11:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function sum(a, b){
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return NaN;
+        } else {
+        
     return a + b;
+        }
 }
 
 module.exports = {
