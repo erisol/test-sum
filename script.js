bbd69b9e3e5e95a9b95e28361e51b866b85adf02
erisol <e.solhaug@stud.uis.no>
2018-10-16 09:21:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function sum(a, b){
-    return a + b;
+    if (typeof a !== number || typeof b !== number) {
+        return NaN;
+    } else {
+        return a + b;
+    }
 }
 
 module.exports = {
